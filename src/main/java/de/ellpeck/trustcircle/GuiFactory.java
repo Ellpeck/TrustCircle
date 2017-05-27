e23e6a7576d622b28b59634913a58b1430a79904
Ellpeck <megamaximal@gmail.com>
2017-05-27 13:12:00 +0200

    do the 1.11 stuff

--- src/main/java/de/ellpeck/trustcircle/GuiFactory.java
+++ src/main/java/de/ellpeck/trustcircle/GuiFactory.java
@@ -20,6 +20,16 @@ public class GuiFactory implements IModGuiFactory{
     }
 
     @Override
+    public boolean hasConfigGui(){
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen){
+        return new ConfigGui(parentScreen);
+    }
+
+    @Override
     public Class<? extends GuiScreen> mainConfigGuiClass(){
         return ConfigGui.class;
     }
